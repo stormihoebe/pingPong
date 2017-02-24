76d734e2c3b9e54f529409a9fef74b4d36e7fda9
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-02-24 09:00:58 -0800

    identify numbers divisible by 3 and write PINT THIS IS A THREE

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,23 @@ var outputArray = [];
 $(function(){
   $("#pingForm").submit(function(){
     event.preventDefault();
+    outputArray = [];
+    $(".outputList").text("");
     var inputNumber = parseInt($("input#inputNumber").val());
     for (index = 1; index <= inputNumber; index++) {
+      if (index%3===0){
+        outputArray.push("PING THIS IS A THREE");
+      } else {
       outputArray.push(index);
+      };
+    // for (index = 3; index <= inputNumber; index +=3){
+    //   outputArray.push(index);
+    //   console.log(outputArray);
     };
-    console.log(outputArray);
+
 
     outputArray.forEach(function(output){
-      $(".outputList").append("<li>" + output + "</li>")
-
-    });
-  });//submit
-});//jQuery
+      $(".outputList").append("<li>" + output + "</li>");
+    });//output array apprend
+  });//end submit
+});//end jQuery
