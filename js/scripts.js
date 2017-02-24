5c5d8939f68886f95ba31c87a330c393078f8e3d
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-02-24 09:10:08 -0800

    identify numbers divisible by by 3, 5, and 15 and return ping, pong, and ping-pong respectively

--- js/scripts.js
+++ js/scripts.js
@@ -11,11 +11,11 @@ $(function(){
     var inputNumber = parseInt($("input#inputNumber").val());
     for (index = 1; index <= inputNumber; index++) {
       if ((index%5===0) && (index%3===0)){
-        outputArray.push("PONGPONG THIS IS A FIFTEEN DIVISIBLE BY 3 and 5!");
+        outputArray.push("ping-pong");
       } else if (index%5===0){
-        outputArray.push("PONG THIS IS A FIVE");
+        outputArray.push("pong");
       } else if (index%3===0){
-        outputArray.push("PING THIS IS A THREE");
+        outputArray.push("ping");
       } else {
         outputArray.push(index);
 
